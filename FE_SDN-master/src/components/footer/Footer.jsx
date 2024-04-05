9ba2851c42cc2fadf5efe420ea7d1ebdf4998b3b
Doan Son <sondshe176797@fpt.edu.vn>
2024-04-05 17:38:50 +0700

    update

--- FE_SDN-master/src/components/footer/Footer.jsx
+++ FE_SDN-master/src/components/footer/Footer.jsx
@@ -2,53 +2,131 @@ import React from 'react';
 import './Footer.css';
 const Footer = () => {
     return (
-        <div class="footer">
+        <footer class="footer">
             <div class="container">
-                <div class="row k15">
-                    <div class="col-xl-4 khoi15">
-                        <div class="logo15">
-                            <img src="img/3.png" alt="" />
+                <div class="row">
+                    <div class="col-12 col-sm-8 offset-sm-2 col-md-3 offset-md-0">
+                        <div class="intro">
+                            <img src="https://vietpost.com.au/_next/static/logo-header-d003d2174319c9c12a4071ecb632a963.svg"
+                                alt="" />
+                            <div class="desc">Vietpost là dịch vụ Thương mại điện tử &amp;Logistic của Vpost. Đây là
+                                dịch vụ vận chuyển quốc tế mang đến cho bạn lựa chọn mua sắm thoải mái và chuyển các mặt
+                                hàng từ các cửa hàng của Úc, Trung Quốc, Nhật Bản, Châu Âu và Hoa Kỳ về Việt Nam với mức
+                                giá cạnh tranh!</div>
                         </div>
-                        <p class="khoi15">
-                        Năm 2020, ShopDunk trở thành đại lý ủy quyền của Apple. Chúng tôi phát triển chuỗi cửa hàng 
-                        tiêu chuẩn và Apple Mono Store nhằm mang đến trải nghiệm tốt nhất về sản phẩm và dịch vụ của
-                         Apple cho người dùng Việt Nam.
-                        </p>
-
                     </div>
-
-                    <div class="col-xl-2 khoi15">
-                        <a>THÔNG TIN</a><br />
-                        <a>Tin tức</a><br />
-                        <a>Giới thiệu</a><br />
-                        <a>Check IMEI</a><br />
-                        <a>Thuê điểm bán lẻ</a><br />
-                        <a>Bảo hành và sửa chữa</a><br />
+                    <div class="col-12 col-sm-8 offset-sm-2 col-md-3 offset-md-0">
+                        <div class="heading">Chính sách</div>
+                        <ul class="list-unstyled links-list">
+                            <li>
+                                <a class="gf-link" href="#">
+                                    <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
+                                        alt="" />
+                                    <span>Chính sách đền bù</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="gf-link" href="#">
+                                    <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
+                                        alt="" />
+                                    <span>Vận chuyển Việt - Úc</span>
+                                </a>
+                            </li>S
+                            <li>
+                                <a class="gf-link" href="#">
+                                    <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
+                                        alt="" />
+                                    <span>Hướng dẫn đăng ký tài khoản</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="gf-link" href="#">
+                                    <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
+                                        alt="" />
+                                    <span>Hướng dẫn tạo vận đơn</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="gf-link" href="#">
+                                    <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
+                                        alt="" />
+                                    <span>Chính sách riêng tư</span>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
-
-
-                    <div class="col-xl-2 khoi15">
-                        <a>Chính Sách</a><br />
-                        <a>Thu cũ đổi mới</a><br />
-                        <a>Giao Hàng</a><br />
-                        <a>Huỷ giao dịch</a><br />
-                        <a>Đổi trả</a><br />
-                        <a>Bảo Hành</a><br />
-
+                    <div class="col-12 col-sm-8 offset-sm-2 col-md-3 offset-md-0">
+                        <div class="heading">Về chúng tôi</div>
+                        <ul class="list-unstyled links-list">
+                            <li>
+                                <a class="gf-link" href="https://zalo.me/g/zaqhuu493">
+                                    <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
+                                        alt="" />
+                                    <span>Group hỗ trợ zalo</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="gf-link" href="https://tocamart.com">
+                                    <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
+                                        alt="" />
+                                    <span>Dịch vụ Order hàng Úc</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="gf-link" href="/#how-it-works">
+                                    <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
+                                        alt="" />
+                                    <span>Phương thức hoạt động</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="gf-link" href="/#about-us">
+                                    <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
+                                        alt="" />
+                                    <span>Giới thiệu về Vietpost</span>
+                                </a>
+                            </li>
+                        </ul>
                     </div>
-                   
-                    <div class="col-xl-2 khoi15">
-                        <a>Địa chỉ & liên hệ</a><br />
-                        <a>Tài Khoản của tôi</a><br />
-                        <a>Đơn đặt hàng</a><br />
-                        <a>Hệ thống cửa hàng</a><br />
-                        <a>Doanh nghiệp:<b style={{color:'#0066cc'}}>0822.688.668</b></a><br />
-
+                    <div class="col-12 col-sm-8 offset-sm-2 col-md-3 offset-md-0">
+                        <div class="heading">An Inch Aways</div>
+                        <div class="address">
+                            <p>
+                                VietPost Pty Ltd<br />Lot. 06 , 14-16 Belmore Road, Punchbowl NSW 2196
+                            </p>
+                            <p>Tel:
+                                0451818366
+                            </p>
+                            <p>
+                                Email:
+                                <a href="#" class="__cf_email__"
+                                    data-cfemail="721d1c1e1b1c1732041b1706021d01065c111d1f5c1307">essg@gmail.com</a>
+                            </p>
+                            <p>8am to 6pm</p>
+                            <p>7 Days a Week</p>
+                        </div>
                     </div>
-
+                </div>
+                <div class="col-12 payment">
+                    <img src="https://vietpost.com.au/_next/static/visa-14b038ba4bc83aa4cb762a85c87652ea.svg"
+                        alt="Visa" />
+                    <img src="https://vietpost.com.au/_next/static/mastercard-292021d0fa9109e19ed85e37f54143c8.svg"
+                        alt="Visa" />
+                    <img src="https://vietpost.com.au/_next/static/paypal-4d8f620a87eb3a2748cce6c788c88627.svg"
+                        alt="Visa" />
+                </div>
+                <div class="col-12 col-sm-8 offset-sm-2 col-md-3 offset-md-9 share">
+                    <img src="https://vietpost.com.au/_next/static/facebook-6909aae3cd905604189e27e2d47facda.svg"
+                        alt="Facebook" />
+                    <img src="https://vietpost.com.au/_next/static/twitter-ab343e12db6f3e59ed0fef9338c5cc1a.svg"
+                        alt="Twitter" />
+                    <img src="https://vietpost.com.au/_next/static/linked-in-1e9e773028fcbc2b1b3db43402e7e849.svg"
+                        alt="Linked In" />
+                    <img src="https://vietpost.com.au/_next/static/instargram-a926322e396c031ffaf7708af6a95f45.svg"
+                        alt="Instagram" />
                 </div>
             </div>
-        </div>
+        </footer>
 
     );
 };
