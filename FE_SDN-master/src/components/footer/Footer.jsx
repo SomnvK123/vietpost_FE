858b62977d48fe5b24ee008d22d5b2e1e189a88e
Doan Son <sondshe176797@fpt.edu.vn>
2024-04-12 18:02:22 +0700

    update

--- FE_SDN-master/src/components/footer/Footer.jsx
+++ FE_SDN-master/src/components/footer/Footer.jsx
@@ -2,52 +2,52 @@ import React from 'react';
 import './Footer.css';
 const Footer = () => {
     return (
-        <footer class="footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col-12 col-sm-8 offset-sm-2 col-md-3 offset-md-0">
-                        <div class="intro">
+        <footer className="footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-12 col-sm-8 offset-sm-2 col-md-3 offset-md-0">
+                        <div className="intro">
                             <img src="https://vietpost.com.au/_next/static/logo-header-d003d2174319c9c12a4071ecb632a963.svg"
                                 alt="" />
-                            <div class="desc">Vietpost là dịch vụ Thương mại điện tử &amp;Logistic của Vpost. Đây là
+                            <div className="desc">Vietpost là dịch vụ Thương mại điện tử &amp;Logistic của Vpost. Đây là
                                 dịch vụ vận chuyển quốc tế mang đến cho bạn lựa chọn mua sắm thoải mái và chuyển các mặt
                                 hàng từ các cửa hàng của Úc, Trung Quốc, Nhật Bản, Châu Âu và Hoa Kỳ về Việt Nam với mức
                                 giá cạnh tranh!</div>
                         </div>
                     </div>
-                    <div class="col-12 col-sm-8 offset-sm-2 col-md-3 offset-md-0">
-                        <div class="heading">Chính sách</div>
-                        <ul class="list-unstyled links-list">
+                    <div className="col-12 col-sm-8 offset-sm-2 col-md-3 offset-md-0">
+                        <div className="heading">Chính sách</div>
+                        <ul className="list-unstyled links-list">
                             <li>
-                                <a class="gf-link" href="#">
+                                <a className="gf-link" href="#">
                                     <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
                                         alt="" />
                                     <span>Chính sách đền bù</span>
                                 </a>
                             </li>
                             <li>
-                                <a class="gf-link" href="#">
+                                <a className="gf-link" href="#">
                                     <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
                                         alt="" />
                                     <span>Vận chuyển Việt - Úc</span>
                                 </a>
                             </li>
                             <li>
-                                <a class="gf-link" href="#">
+                                <a className="gf-link" href="#">
                                     <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
                                         alt="" />
                                     <span>Hướng dẫn đăng ký tài khoản</span>
                                 </a>
                             </li>
                             <li>
-                                <a class="gf-link" href="#">
+                                <a className="gf-link" href="#">
                                     <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
                                         alt="" />
                                     <span>Hướng dẫn tạo vận đơn</span>
                                 </a>
                             </li>
                             <li>
-                                <a class="gf-link" href="#">
+                                <a className="gf-link" href="#">
                                     <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
                                         alt="" />
                                     <span>Chính sách riêng tư</span>
@@ -55,32 +55,32 @@ const Footer = () => {
                             </li>
                         </ul>
                     </div>
-                    <div class="col-12 col-sm-8 offset-sm-2 col-md-3 offset-md-0">
-                        <div class="heading">Về chúng tôi</div>
-                        <ul class="list-unstyled links-list">
+                    <div className="col-12 col-sm-8 offset-sm-2 col-md-3 offset-md-0">
+                        <div claclassNamess="heading">Về chúng tôi</div>
+                        <ul className="list-unstyled links-list">
                             <li>
-                                <a class="gf-link" href="https://zalo.me/g/zaqhuu493">
+                                <a claclassNamess="gf-link" href="https://zalo.me/g/zaqhuu493">
                                     <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
                                         alt="" />
                                     <span>Group hỗ trợ zalo</span>
                                 </a>
                             </li>
                             <li>
-                                <a class="gf-link" href="https://tocamart.com">
+                                <a className="gf-link" href="https://tocamart.com">
                                     <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
                                         alt="" />
                                     <span>Dịch vụ Order hàng Úc</span>
                                 </a>
                             </li>
                             <li>
-                                <a class="gf-link" href="/#how-it-works">
+                                <a className="gf-link" href="/#how-it-works">
                                     <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
                                         alt="" />
                                     <span>Phương thức hoạt động</span>
                                 </a>
                             </li>
                             <li>
-                                <a class="gf-link" href="/#about-us">
+                                <a className="gf-link" href="/#about-us">
                                     <img src="https://vietpost.com.au/_next/static/right-angle-c4b7d5566fb4b356822df3dd87556b1f.svg"
                                         alt="" />
                                     <span>Giới thiệu về Vietpost</span>
@@ -88,9 +88,9 @@ const Footer = () => {
                             </li>
                         </ul>
                     </div>
-                    <div class="col-12 col-sm-8 offset-sm-2 col-md-3 offset-md-0">
-                        <div class="heading">An Inch Aways</div>
-                        <div class="address">
+                    <div claclassNamess="col-12 col-sm-8 offset-sm-2 col-md-3 offset-md-0">
+                        <div className="heading">An Inch Aways</div>
+                        <div className="address">
                             <p>
                                 VietPost Pty Ltd<br />Lot. 06 , 14-16 Belmore Road, Punchbowl NSW 2196
                             </p>
@@ -107,7 +107,7 @@ const Footer = () => {
                         </div>
                     </div>
                 </div>
-                <div class="col-12 payment">
+                <div className="col-12 payment">
                     <img src="https://vietpost.com.au/_next/static/visa-14b038ba4bc83aa4cb762a85c87652ea.svg"
                         alt="Visa" />
                     <img src="https://vietpost.com.au/_next/static/mastercard-292021d0fa9109e19ed85e37f54143c8.svg"
@@ -115,7 +115,7 @@ const Footer = () => {
                     <img src="https://vietpost.com.au/_next/static/paypal-4d8f620a87eb3a2748cce6c788c88627.svg"
                         alt="Visa" />
                 </div>
-                <div class="col-12 col-sm-8 offset-sm-2 col-md-3 offset-md-9 share">
+                <div className="col-12 col-sm-8 offset-sm-2 col-md-3 offset-md-9 share">
                     <img src="https://vietpost.com.au/_next/static/facebook-6909aae3cd905604189e27e2d47facda.svg"
                         alt="Facebook" />
                     <img src="https://vietpost.com.au/_next/static/twitter-ab343e12db6f3e59ed0fef9338c5cc1a.svg"
