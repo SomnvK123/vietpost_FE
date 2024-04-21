4974682286c5df2776aebfad9df94366a40585d9
Doan Son <sondshe176797@fpt.edu.vn>
2024-04-21 14:33:25 +0700

    update

--- FE_SDN-master/src/components/home_po_lookup/Lookup.jsx
+++ FE_SDN-master/src/components/home_po_lookup/Lookup.jsx
@@ -234,7 +234,7 @@ const Lookup = () => {
                                                         padding: '0',
                                                         color: 'inherit',
                                                         overflowY: 'auto',
-                                                        appearance: 'none', // Thêm thuộc tính này để ẩn dấu nhọn mặc định của select
+                                                        appearance: 'none',
                                                     }}
                                                 >
                                                     {selectedProvince ? null : <option value="" style={{ width: '100%' }}></option>}
@@ -242,15 +242,12 @@ const Lookup = () => {
                                                         <option key={province.id} value={province.name}>{province.name}</option>
                                                     ))}
                                                 </select>
-                                                {/* Đặt IndicatorSvg ngay sau select để dễ dàng điều chỉnh vị trí */}
                                                 <IndicatorSvg height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" style={{ position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)', pointerEvents: 'none' }}>
                                                     <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
                                                 </IndicatorSvg>
                                             </div>
                                         </div>
-                                        {/* Loại bỏ IndicatorContainer để tránh trùng lặp */}
                                     </Control>
-
                                 </Containerr>
                             </Form.Group>
                             <Form.Group>
